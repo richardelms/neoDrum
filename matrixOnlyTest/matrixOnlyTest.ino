1478a0e5642a7fddcdb234202207d76f15505034
Richard Elms <richardelms@gmail.com>
2016-12-04 21:24:56 +0100

    matrix ready are refining

--- matrixOnlyTest/matrixOnlyTest.ino
+++ matrixOnlyTest/matrixOnlyTest.ino
@@ -5,7 +5,8 @@
 #include <Adafruit_NeoPixel.h>
 
 //neoPixel definitions
-#define neoOutput 10
+#define neoOutput 3
+#define matrixBrightness 150
 Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(8, 8, neoOutput,
   NEO_MATRIX_BOTTOM     + NEO_MATRIX_LEFT +
   NEO_MATRIX_ROWS + NEO_MATRIX_PROGRESSIVE,
@@ -26,7 +27,7 @@ NeoColor::NeoColor(int red,int green, int blue){
 NeoColor normalStepColor = NeoColor(255,255,255);
 NeoColor selectedColor = NeoColor(0,255,0);
 NeoColor selectedAndStepColor = NeoColor(0,0,255);
-NeoColor baseUnselectedValue = NeoColor(100,100,100);
+NeoColor baseUnselectedValue = NeoColor(0,0,0);
 
 //config values
 int numSteps = 8;
@@ -34,40 +35,29 @@ int bpm = 120;
 bool firstStep = true;
 
 //util vars
-int step = 0;
-int selectedInstrument = 0;
+int currentStep = 0;
+int selectedInstrument = 4;
 unsigned long lastStep = 0;
 
-bool sequence[8][8] = {{false,false,false,false,false,false,false,false},
+bool sequence[8][8] = {{true,true,false,false,true,false,false,false},
+            {false,true,false,false,true,false,false,false},
+            {false,true,false,true,false,true,false,true},
             {false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false}};
+            {false,false,true,false,false,false,false,true},
+            {true,false,false,false,true,true,true,false},
+            {false,false,true,true,false,false,false,false},
+            {true,true,false,false,false,false,false,true}};
 
 
 // initialisation
 void setup() {
   InitMatrix();
-  InitAnimation();
 }
 
 void InitMatrix(){
   matrix.begin();
-  matrix.setBrightness(255);
+  matrix.setBrightness(matrixBrightness);
   matrix.show();
-}
-
-void InitAnimation(){
-  for(int x = 0; x < 4; x++){
-    for(int i = 0 ; i < 64; i++){
-      matrix.setPixelColor(i, random(1,255), random(1,255), random(1,255));
-      matrix.show();
-      delay(100);
-    }
-  }
 }
 
 // program loop
@@ -75,15 +65,14 @@ void loop() {
   CheckForStep();
 }
 
-
 void CheckForStep(){
   unsigned long currentMillis = millis();
   if(!firstStep){
     if (currentMillis - lastStep >= 60000/bpm) {
       lastStep = currentMillis;
-      step ++;
-      if(step == numSteps){
-        step = 0;
+      currentStep ++;
+      if(currentStep == numSteps){
+        currentStep = 0;
       }
       MakeStep();
     }
@@ -99,50 +88,27 @@ void MakeStep(){
 }
 
 void UpdateLeds(){
-  clearAllLeds();   
-  ShowCurrentStep();
-  setupSelectedLeds();
-  matrix.show();
-}
-
-void clearAllLeds(){
-  for(int i = 0; i < 64; i++){
-    matrix.setPixelColor(i, baseUnselectedValue.r, baseUnselectedValue.g, baseUnselectedValue.b);
-  }
-}
-
-void ShowCurrentStep(){
-  int masterIndex = 0;
-  for(int i = 0; i < 8; i++){
-    for(int x = 0; x < 8; x ++){
-      if(step == x){
-        if(sequence[i][x] == true){
-          matrix.setPixelColor(masterIndex, selectedAndStepColor.r, selectedAndStepColor.g, selectedAndStepColor.b);
-        }else{
-          matrix.setPixelColor(masterIndex, normalStepColor.r, normalStepColor.g, normalStepColor.b);
-        }
-      }
-      masterIndex++;
-    }
-  }
-}
-
-void setupSelectedLeds(){
   int masterIndex = 0;
   for(int i = 0; i < 8; i++){
     for(int x = 0; x < 8; x ++){
       if(sequence[i][x] == true){
-        if(selectedInstrument == i){
+        if(currentStep == x){
+          matrix.setPixelColor(masterIndex, selectedAndStepColor.r, selectedAndStepColor.g, selectedAndStepColor.b);
+        }else{
+          matrix.setPixelColor(masterIndex, selectedColor.r, selectedColor.g, selectedColor.b);
+        }
+      }else{
+        if(currentStep == x){
+          matrix.setPixelColor(masterIndex, normalStepColor.r, normalStepColor.g, normalStepColor.b);
+        }else{
           matrix.setPixelColor(masterIndex, baseUnselectedValue.r, baseUnselectedValue.g, baseUnselectedValue.b);
         }
-      }
+      } 
       masterIndex++;
-    }
+    } 
   }
+  matrix.show();
 }
-
-
-
 
 
 
