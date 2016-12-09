cac18680c95009cc9d70dabc19daf583394f6159
Richard Elms <richardelms@gmail.com>
2016-12-09 10:18:22 +0100

    IT WORKS!!!!

--- master/master.ino
+++ master/master.ino
@@ -10,18 +10,17 @@
 
 //input definitions
 //pots
-Potentiometer brightnessPot = Potentiometer(2);
-int lastBrightnessLevel = 0;
+Potentiometer brightnessPot = Potentiometer(15);
 
 //instrumentbuttons
-Pushbutton instrumentSelect_0(31);
-Pushbutton instrumentSelect_1(33);
-Pushbutton instrumentSelect_2(35);
-Pushbutton instrumentSelect_3(37);
-Pushbutton instrumentSelect_4(39);
-Pushbutton instrumentSelect_5(41);
-Pushbutton instrumentSelect_6(43);
-Pushbutton instrumentSelect_7(45);
+Pushbutton instrumentSelect_0(30);
+Pushbutton instrumentSelect_1(32);
+Pushbutton instrumentSelect_2(34);
+Pushbutton instrumentSelect_3(36);
+Pushbutton instrumentSelect_4(38);
+Pushbutton instrumentSelect_5(40);
+Pushbutton instrumentSelect_6(42);
+Pushbutton instrumentSelect_7(44);
 Pushbutton instrumentSelectButtons[] = {
   instrumentSelect_0,instrumentSelect_1,instrumentSelect_2,
   instrumentSelect_3,instrumentSelect_4,instrumentSelect_5,
@@ -29,26 +28,24 @@ Pushbutton instrumentSelectButtons[] = {
 };
 
 //hitButtons
-Pushbutton hitButton_0(30);
-Pushbutton hitButton_1(32);
-Pushbutton hitButton_2(34);
-Pushbutton hitButton_3(36);
-Pushbutton hitButton_4(38);
-Pushbutton hitButton_5(40);
-Pushbutton hitButton_6(42);
-Pushbutton hitButton_7(44);
+Pushbutton hitButton_0(31);
+Pushbutton hitButton_1(33);
+Pushbutton hitButton_2(35);
+Pushbutton hitButton_3(37);
+Pushbutton hitButton_4(39);
+Pushbutton hitButton_5(41);
+Pushbutton hitButton_6(43);
+Pushbutton hitButton_7(45);
 Pushbutton hitButtons[] = {
   hitButton_0,hitButton_1,hitButton_2,
   hitButton_3,hitButton_4,hitButton_5,
   hitButton_6,hitButton_7
 };
 
-
-
 //neoPixel definitions
 #define neoOutput 3
 Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(8, 8, neoOutput,
-  NEO_MATRIX_BOTTOM     + NEO_MATRIX_LEFT +
+  NEO_MATRIX_TOP     + NEO_MATRIX_LEFT +
   NEO_MATRIX_ROWS + NEO_MATRIX_PROGRESSIVE,
   NEO_GRB            + NEO_KHZ800);
 class NeoColor
@@ -77,63 +74,107 @@ NeoColor selectedInstrumentColor = NeoColor(0,0,255);
 
 //midi definitions
 MIDI_CREATE_DEFAULT_INSTANCE();
-int instrumentNotes[] = {36,38,39,42,46,43,45,48};
+int instrumentNotes[] = {43,42,41,40,39,38,37,36};
 
 //config values
 int numSteps = 8;
-int bpm = 120;
+int bpm = 150;
 bool firstStep = true;
-int matrixBrightness = 50;
+int matrixStartingBrightness = 10;
+int lastBrightnessLevel = 0;
 
 //util vars
 int currentStep = 0;
-int selectedInstrument = 4;
+int selectedInstrument = 7;
 unsigned long lastStep = 0;
-
-bool sequence[8][8] = {{false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false}};
-
+bool sequence[8][8];
 
 // initialisation
 void setup() {
+  Serial.begin(9600);
   MIDI.begin(MIDI_CHANNEL_OMNI);
   InitMatrix();
 }
 
 void InitMatrix(){
-  brightnessPot.setSectors(255);
+//  brightnessPot.setSectors(255);
   matrix.begin();
-  matrix.setBrightness(matrixBrightness);
+  matrix.setBrightness(matrixStartingBrightness);
   matrix.show();
 }
 
 // program loop
 void loop() {
-  CheckForInputs();
   CheckForStep();
-  CheckForBrightnessAdjustment();
+    CheckForInputs();
+
+//  CheckForBrightnessAdjustment();
 }
 
 void CheckForInputs(){  
-  for(int i = 0 ; i < sizeof(instrumentSelectButtons);i++){
-        if(instrumentSelectButtons[i].getSingleDebouncedPress()){
-              selectedInstrument = i;
-              UpdateLeds();
+          if(instrumentSelectButtons[0].getSingleDebouncedRelease()){
+              SetInstrument(0);
           }
-    }
-    for(int i = 0 ; i < sizeof(hitButtons);i++){
-        if(hitButtons[i].getSingleDebouncedPress()){
-              sequence[selectedInstrument][i] = !sequence[selectedInstrument][i];
-              UpdateLeds();
+          if(instrumentSelectButtons[1].getSingleDebouncedRelease()){
+              SetInstrument(1);
           }
-    }
+          if(instrumentSelectButtons[2].getSingleDebouncedRelease()){
+              SetInstrument(2);
+          }
+          if(instrumentSelectButtons[3].getSingleDebouncedRelease()){
+              SetInstrument(3);
+          }
+          if(instrumentSelectButtons[4].getSingleDebouncedRelease()){
+              SetInstrument(4);
+          }
+          if(instrumentSelectButtons[5].getSingleDebouncedRelease()){
+              SetInstrument(5);
+          }
+          if(instrumentSelectButtons[6].getSingleDebouncedRelease()){
+              SetInstrument(6);
+          }
+          if(instrumentSelectButtons[7].getSingleDebouncedRelease()){
+              SetInstrument(7);
+          }
+
+
+
+         if(hitButtons[0].getSingleDebouncedRelease()){
+              SetHit(0);
+          }
+           if(hitButtons[1].getSingleDebouncedRelease()){
+              SetHit(1);
+          }
+           if(hitButtons[2].getSingleDebouncedRelease()){
+              SetHit(2);
+          }
+           if(hitButtons[3].getSingleDebouncedRelease()){
+              SetHit(3);
+          }
+           if(hitButtons[4].getSingleDebouncedRelease()){
+              SetHit(4);
+          }
+           if(hitButtons[5].getSingleDebouncedRelease()){
+              SetHit(5);
+          }
+           if(hitButtons[6].getSingleDebouncedRelease()){
+              SetHit(6);
+          }
+           if(hitButtons[7].getSingleDebouncedRelease()){
+              SetHit(7);
+          }
 }
+
+void SetInstrument(int instrument){
+    selectedInstrument = instrument;
+    UpdateLeds();
+    MIDI.sendNoteOn(instrumentNotes[instrument], 100, 1);  
+  }
+
+  void SetHit(int x){
+    sequence[selectedInstrument][x] = !sequence[selectedInstrument][x];
+    UpdateLeds();
+  }
 
 void CheckForBrightnessAdjustment(){
     int brightnessPotValue = brightnessPot.getSector();
